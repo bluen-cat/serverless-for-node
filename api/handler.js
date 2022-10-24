96ac9b428f783ee9006b3574f942d6cd964dc8be
bluen-cat <sky@email.com>
2022-10-25 02:10:05 +0800

    test

--- api/handler.js
+++ api/handler.js
@@ -1,7 +1,3 @@
 export default function handler(request, response) {
-    return response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-    });
+    response.send("132456");
 }
